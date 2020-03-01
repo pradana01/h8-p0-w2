8faa2065ce03494949e4c692e2f34688f7828763
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-03-01 14:43:35 +0700

    commit push
    latihan jumat balik kata

---
+++ Balik-Kata.js
@@ -0,0 +1,9 @@
+function balikKata(kata){
+    return kata.split('').reverse().join('');
+}
+
+console.log(balikKata('Hello World and Coders'));
+console.log(balikKata('John Doe'));
+console.log(balikKata('I am a bookworm'));
+console.log(balikKata('Coding is my hobby'));
+console.log(balikKata('Super'));
