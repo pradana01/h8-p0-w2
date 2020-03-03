5512c85865668ea2df32367fc0c1f6e2f20b1b65
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-03-03 08:35:00 +0700

    commit push
    Revisi tugas balik kata

--- Balik-Kata.js
+++ Balik-Kata.js
@@ -1,9 +1,8 @@
 function balikKata(kata){
-    return kata.split('').reverse().join('');
+    var letter = '';
+    for (var i = kata.length - 1; i >= 0; i--){
+        letter += kata[i];
+    } return letter;
 }
 
-console.log(balikKata('Hello World and Coders'));
-console.log(balikKata('John Doe'));
-console.log(balikKata('I am a bookworm'));
-console.log(balikKata('Coding is my hobby'));
-console.log(balikKata('Super'));
+console.log(balikKata('andrean'));
